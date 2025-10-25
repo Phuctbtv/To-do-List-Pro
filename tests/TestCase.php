542b7d228cb42e7de2228f9e5106263d32d2c788
Phạm Đại Phúc <167066175+Phuctbtv@users.noreply.github.com>
2025-10-25 13:16:47 +0700

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Vite; // <-- Thêm dòng này
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    /**
+     * Sets up the tests.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Vite::preventStubs(); // <-- Dòng này sẽ sửa lỗi của bạn
+    }
 }
