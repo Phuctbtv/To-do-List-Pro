e18f8aa787503767b0ff4696543399713bb85081
Phuctbtv <167066175+Phuctbtv@users.noreply.github.com>
2025-11-18 00:30:27 +0700

    Complete reading and add job functions

---
+++ database/migrations/2025_11_17_154917_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');//tiêu đề
+            $table->text('description')->nullable();//mô tả
+            $table->boolean('status')->default(0);//trạng thái
+            $table->date('due_date')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
